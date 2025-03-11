d28a91fb67389559ffa99bd69a010be949aa21b9
Jhonatan Bergmann <54135650+jhonbergmann@users.noreply.github.com>
2025-03-11 20:24:07 -0300

    app

--- app.tsx
+++ app.tsx
@@ -6,15 +6,15 @@ import {CarouselBanner} from './src/components/carousel-banners'
 const sliders = [
   {
     id: 1,
-    img: 'https://randomwordgenerator.com/img/picture-generator/54e9d4464357ab14f1dc8460962e33791c3ad6e04e507440772d73d69e44cc_640.jpg',
+    img: 'https://raw.githubusercontent.com/jhonbergmann/simple-carousel-banners/refs/heads/main/src/assets/slide1.jpg',
   },
   {
     id: 2,
-    img: 'https://randomwordgenerator.com/img/picture-generator/54e4d4454c57b10ff3d8992cc12c30771037dbf8525478497d2873d7934f_640.jpg',
+    img: 'https://raw.githubusercontent.com/jhonbergmann/simple-carousel-banners/refs/heads/main/src/assets/slide2.jpg',
   },
   {
     id: 3,
-    img: 'https://randomwordgenerator.com/img/picture-generator/5ee2dd4a4e57b10ff3d8992cc12c30771037dbf85254794e73287add9449_640.jpg',
+    img: 'https://raw.githubusercontent.com/jhonbergmann/simple-carousel-banners/refs/heads/main/src/assets/slide3.jpg',
   },
 ]
 
